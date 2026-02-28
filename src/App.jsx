13240688d0b8233934cbb0d2e6f86e3577188f5c
Giannis Ersov <giannis.ersov@gmail.com>
2026-02-28 23:55:10 +0200

    usereducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import { useState, useMemo } from 'react';
 
 export default function App() {
-  const [count, setCount] = useState(0);
-
-  const expensiveValue = useMemo(() => {
-    let sum = 0;
-    for (let i = 0; i < 1000000000; i++) sum += i;
-    return sum;
-  }, []); 
+  
 
   return (
-    <button onClick={() => setCount(count + 1)}>
-      Count: {count} | Math: {expensiveValue}
-    </button>
+    <></>
   );
 }

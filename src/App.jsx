dc5ec0d39bb9d46503394f27b2859d111595b983
Giannis Ersov <giannis.ersov@gmail.com>
2026-03-02 10:23:34 +0200

    UseReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useReducer } from 'react';
 
 export default function App() {
-  
+    const [] = useReducer(useReducer, { count : 0});
 
   return (
     <></>
